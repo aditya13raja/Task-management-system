33ca9f61bafdad25b2759843c14179c6f37ed21e
aditya13raja <aditya13raja@gmail.com>
2024-08-27 16:11:26 +0530

    Implement task list component

--- task-management-app/src/app/components/task-list/task-list.component.ts
+++ task-management-app/src/app/components/task-list/task-list.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-list',
-  standalone: true,
-  imports: [],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+  styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
 
+  @Output() editTask = new EventEmitter<Task>();
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.taskService.getTasks().then((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+
+  onEditTask(task: Task) {
+    this.editTask.emit(task);
+  }
+
+  onDeleteTask(id: string) {
+    this.taskService.deleteTask(id).then(() => {
+      this.tasks = this.tasks.filter(task => task.id !== id);
+    });
+  }
 }
