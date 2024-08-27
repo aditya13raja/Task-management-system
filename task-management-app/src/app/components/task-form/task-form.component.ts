1acfe63b631befa85fb79df945bdfe6e6782a013
aditya13raja <aditya13raja@gmail.com>
2024-08-27 15:39:01 +0530

    Add task form component

--- task-management-app/src/app/components/task-form/task-form.component.ts
+++ task-management-app/src/app/components/task-form/task-form.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-form',
-  standalone: true,
-  imports: [],
   templateUrl: './task-form.component.html',
-  styleUrl: './task-form.component.css'
+  styleUrls: ['./task-form.component.css']
 })
 export class TaskFormComponent {
+  @Input() task: Task = {
+    id: '',
+    title: '',
+    description: '',
+    dueDate: new Date(),
+    priority: 'medium',
+    status: 'to-do',
+    history: [],
+  };
 
+  @Input() isEditMode = false;
+  @Output() taskSaved = new EventEmitter<Task>();
+
+  constructor(private taskService: TaskService) {}
+
+  onSubmit() {
+    if (this.isEditMode) {
+      this.taskService.updateTask(this.task).then((updatedTask) => {
+        this.taskSaved.emit(updatedTask);
+      });
+    } else {
+      this.taskService.createTask(this.task).then((newTask) => {
+        this.taskSaved.emit(newTask);
+      });
+    }
+  }
 }
